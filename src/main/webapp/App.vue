1953cc93f16bfbfab33def10d2709e02f69aaefc
Victor Malyshev <vmalyshev0@gmail.com>
2019-12-07 13:10:12 +0200

    Added and configured Vuetify and Vuex

--- src/main/webapp/App.vue
+++ src/main/webapp/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+
+export default {};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/main/webapp/VuetifyConf.js
@@ -0,0 +1,14 @@
+/**
+ * @author victor
+ * @date 07.12.2019
+ * @project: pubslist
+ */
+
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+
+Vue.use(Vuetify);
+
+const opts = {};
+
+export default new Vuetify(opts);
