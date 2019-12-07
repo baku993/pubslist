b2e6775664c67bc0ec3bf86eb39c2a1d3ec3a8c8
Victor Malyshev <vmalyshev0@gmail.com>
2019-12-07 11:36:28 +0200

    adding vuetify to the project

--- src/main/webapp/router/index.js
+++ src/main/webapp/router/index.js
@@ -1,10 +1,25 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  // Login
+  {
+    path: "*",
+    redirect: "/login"
+  },
+  {
+    path: "/",
+    redirect: "/login"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
   {
     path: "/",
     name: "home",
---
+++ src/main/webapp/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login">
+    <h3>Login</h3>
+    <input type="text" v-model="email" placeholder="Email" /><br />
+    <input type="password" v-model="password" placeholder="Password" /><br />
+    <button class="btn" :click="login"><span>Login</span></button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "login",
+  data: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login: function() {
+      axios
+        .post("/login", { username: this.email, password: this.password })
+        .then(
+          () => {
+            this.$router.replace("home");
+          },
+          err => {
+            alert("Oops. " + err.message);
+          }
+        );
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login {
+  display: block;
+}
+input {
+  margin: 10px 0;
+  padding: 15px;
+  border-radius: 4px;
+}
+.btn {
+  margin-top: 20px;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
