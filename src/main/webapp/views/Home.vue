1058144ae5c08e224bec2f4ffa12b080737e9638
Victor Malyshev <vmalyshev0@gmail.com>
2019-12-11 14:08:00 +0200

    ESlint config

--- src/main/webapp/views/Home.vue
+++ src/main/webapp/views/Home.vue
@@ -1,97 +1,97 @@
 <template>
-  <div class="home">
-    <div class="headline">Your favorite places</div>
-    <div class="tabs">
-      <v-tabs class="tabs__elem d-flex">
-        <v-tab @click="tabSelected = 0">All places</v-tab>
-        <v-tab @click="tabSelected = 1">Your Places</v-tab>
-        <v-tab @click="tabSelected = 2">Pending</v-tab>
-      </v-tabs>
-    </div>
-    <places-list
-      @liked="addToLiked"
-      @vote="voteForPlace"
-      :places="filteredPlaces"
-    ></places-list>
-  </div>
+	<div class='home'>
+		<div class='headline'>Your favorite places</div>
+		<div class='tabs'>
+			<v-tabs class='tabs__elem d-flex'>
+				<v-tab @click='tabSelected = 0'>All places</v-tab>
+				<v-tab @click='tabSelected = 1'>Your Places</v-tab>
+				<v-tab @click='tabSelected = 2'>Pending</v-tab>
+			</v-tabs>
+		</div>
+		<places-list
+				@liked='addToLiked'
+				@vote='voteForPlace'
+				:places='filteredPlaces'
+		></places-list>
+	</div>
 </template>
 
 <script>
-import PlacesList from "../components/PlacesList";
-import authApi from "../auth/authApi";
-import { GET_USER, SET_USER_ACTION } from "../constants";
-import { mapGetters } from "vuex";
+	import PlacesList from '../components/PlacesList';
+	import authApi from '../auth/authApi';
+	import {GET_USER, SET_USER_ACTION} from '../constants';
+	import {mapGetters} from 'vuex';
 
-export default {
-  name: "home",
-  components: { PlacesList },
-  data() {
-    return {
-      tabSelected: 0,
-      places: []
-    };
-  },
-  computed: {
-    ...mapGetters([GET_USER]),
-    filteredPlaces() {
-      let filtered;
-      switch (this.tabSelected) {
-        case 0:
-        default:
-          filtered = this.places;
-          break;
-        case 1:
-          filtered = this.places.filter(
-            p => p.createdBy === this.getUser.username
-          );
-          break;
-        case 2:
-          filtered = this.places.filter(
-            p =>
-              (p.createdBy === this.getUser.username && !p.approved) ||
-              (!p.approved && this.getUser.role.includes("ADMIN"))
-          );
-          break;
-      }
-      return filtered;
-    }
-  },
-  methods: {
-    voteForPlace(id) {
-      console.log("Vote for place", id);
-    },
-    addToLiked(id) {
-      console.log("Add to liked places", id);
-    }
-  },
-  created() {
-    authApi
-      .get("/api/user")
-      .then(async response => {
-        await this.$store.dispatch(SET_USER_ACTION, response.data);
-        await authApi.get("/api/places").then(response => {
-          this.places = response.data;
-        });
-      })
-      .catch(error => {
-        // Add user notification here
-        console.log(error);
-      });
-  }
-};
+	export default {
+		name: 'home',
+		components: {PlacesList},
+		data() {
+			return {
+				tabSelected: 0,
+				places: []
+			};
+		},
+		computed: {
+			...mapGetters([GET_USER]),
+			filteredPlaces() {
+				let filtered;
+				switch (this.tabSelected) {
+					case 0:
+					default:
+						filtered = this.places;
+						break;
+					case 1:
+						filtered = this.places.filter(
+							p => p.createdBy === this.getUser.username
+						);
+						break;
+					case 2:
+						filtered = this.places.filter(
+							p =>
+								(p.createdBy === this.getUser.username && !p.approved) ||
+								(!p.approved && this.getUser.role.includes('ADMIN'))
+						);
+						break;
+				}
+				return filtered;
+			}
+		},
+		methods: {
+			voteForPlace(id) {
+				console.log('Vote for place', id);
+			},
+			addToLiked(id) {
+				console.log('Add to liked places', id);
+			}
+		},
+		created() {
+			authApi
+				.get('/api/user')
+				.then(async response => {
+					await this.$store.dispatch(SET_USER_ACTION, response.data);
+					await authApi.get('/api/places').then(resp => {
+						this.places = resp.data;
+					});
+				})
+				.catch(error => {
+					// Add user notification here
+					console.log(error);
+				});
+		}
+	};
 </script>
 
 <style lang="scss">
-.tabs {
-  margin-top: 50px;
-  padding-bottom: 20px;
-}
+	.tabs {
+		margin-top: 50px;
+		padding-bottom: 20px;
+	}
 
-.tabs__elem {
-  justify-content: center;
+	.tabs__elem {
+		justify-content: center;
 
-  & .v-tabs-bar {
-    background-color: inherit !important;
-  }
-}
+		& .v-tabs-bar {
+			background-color: inherit !important;
+		}
+	}
 </style>
