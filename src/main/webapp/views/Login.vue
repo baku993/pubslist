d4f92f99bad855d7ec745932b7129dbd3b9d552e
Victor Malyshev <vmalyshev0@gmail.com>
2019-12-13 11:13:06 +0200

    Build fix, accidentally broke it

--- src/main/webapp/views/Login.vue
+++ src/main/webapp/views/Login.vue
@@ -1,52 +1,50 @@
 <template>
-  <div class='login'>
-    <v-app>
-      <v-content>
-        <v-container class='fill-height' fluid>
-          <v-row align='center' justify='center'>
-            <v-col cols='12' sm='8' md='4'>
-              <v-card class='elevation-12'>
-                <v-toolbar color='primary' dark flat>
-                  <v-toolbar-title class='headline mb-1'>Login</v-toolbar-title>
-                  <v-spacer />
-                </v-toolbar>
-                <v-card-text>
-                  <v-form v-model='valid' ref='form'>
-                    <v-text-field
-                      label='Login'
-                      name='login'
-                      prepend-icon='person'
-                      type='text'
-                      :rules='[v => !!v || &apos;Password is required&apos;]'
-                      v-model='username'
-                    />
+	<div class='login'>
+		<v-app>
+			<v-content>
+				<v-container class='fill-height' fluid>
+					<v-row align='center' justify='center'>
+						<v-col cols='12' sm='8' md='4'>
+							<v-card class='elevation-12'>
+								<v-toolbar color='primary' dark flat>
+									<v-toolbar-title class='headline mb-1'>Login</v-toolbar-title>
+									<v-spacer/>
+								</v-toolbar>
+								<v-card-text>
+									<v-form v-model='valid' ref='form'>
+										<v-text-field
+												label='Login'
+												name='login'
+												prepend-icon='person'
+												type='text'
+												:rules='[v => !!v || "Username is required"]'
+												v-model='username'/>
 
-                    <v-text-field
-                      label='Password'
-                      name='password'
-                      prepend-icon='lock'
-                      type='password'
-                      :rules='[v => !!v || &apos;Password is required&apos;]'
-                      v-model='password'
-                    />
-                  </v-form>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn text color='green darken-2' @click='goToSignUp'>
-                    New? Sign up
-                  </v-btn>
-                  <v-spacer />
-                  <v-btn :class='{ grey: !valid, blue: valid }' @click='login'>
-                    Login
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-content>
-    </v-app>
-  </div>
+										<v-text-field
+												label='Password'
+												name='password'
+												prepend-icon='lock'
+												type='password'
+												:rules='[v => !!v || "Password is required"]'
+												v-model='password'/>
+									</v-form>
+								</v-card-text>
+								<v-card-actions>
+									<v-btn text color='green darken-2' @click='goToSignUp'>
+										New? Sign up
+									</v-btn>
+									<v-spacer/>
+									<v-btn :class='{ grey: !valid, blue: valid }' @click='login'>
+										Login
+									</v-btn>
+								</v-card-actions>
+							</v-card>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-content>
+		</v-app>
+	</div>
 </template>
 
 <script>
@@ -91,23 +89,4 @@
 </script>
 
 <style scoped>
-.login {
-  display: block;
-}
-
-input {
-  margin: 10px 0;
-  padding: 15px;
-  border-radius: 4px;
-}
-
-p {
-  margin-top: 40px;
-  font-size: 13px;
-}
-
-p a {
-  text-decoration: underline;
-  cursor: pointer;
-}
 </style>
--- src/main/webapp/views/SignUp.vue
+++ src/main/webapp/views/SignUp.vue
@@ -1,102 +1,88 @@
 <template>
-  <div class='login'>
-    <v-app>
-      <v-content>
-        <v-container class='fill-height' fluid>
-          <v-row align='center' justify='center'>
-            <v-col cols='12' sm='8' md='4'>
-              <v-card class='elevation-12'>
-                <v-toolbar color='secondary' dark flat>
-                  <v-toolbar-title class='headline mb-1'
-                    >Sign up</v-toolbar-title
-                  >
-                  <v-spacer />
-                </v-toolbar>
-                <v-card-text>
-                  <v-form v-model='valid' ref='form'>
-                    <v-text-field
-                      label='Username'
-                      name='username'
-                      type='text'
-                      :rules='usernameRules'
-                      counter
-                      hint='More than 6 characters, letters and numbers'
-                      required
-                      v-model='username'
-                    />
+	<div class='login'>
+		<v-app>
+			<v-content>
+				<v-container class='fill-height' fluid>
+					<v-row align='center' justify='center'>
+						<v-col cols='12' sm='8' md='4'>
+							<v-card class='elevation-12'>
+								<v-toolbar color='secondary' dark flat>
+									<v-toolbar-title class='headline mb-1'>Sign up</v-toolbar-title>
+									<v-spacer/>
+								</v-toolbar>
+								<v-card-text>
+									<v-form v-model='valid' ref='form'>
+										<v-text-field
+												label='Username'
+												name='username'
+												type='text'
+												:rules='usernameRules'
+												counter
+												hint='More than 6 characters, letters and numbers'
+												required
+												v-model='username'/>
 
-                    <v-text-field
-                      label='First name'
-                      name='firstname'
-                      type='text'
-                      required
-                      :rules='[v => !!v || &apos;First name is required&apos;]'
-                      v-model='firstName'
-                    />
+										<v-text-field
+												label='First name'
+												name='firstname'
+												type='text'
+												required
+												:rules='[v => !!v || "First name is required"]'
+												v-model='firstName'/>
 
-                    <v-text-field
-                      label='Last name'
-                      name='lastname'
-                      type='text'
-                      required
-                      :rules='[v => !!v || &apos;Last name is required&apos;]'
-                      v-model='lastName'
-                    />
+										<v-text-field
+												label='Last name'
+												name='lastname'
+												type='text'
+												required
+												:rules='[v => !!v || "Last name is required"]'
+												v-model='lastName'/>
 
-                    <v-text-field
-                      label='Password'
-                      name='password'
-                      type='password'
-                      :rules='passwordRules'
-                      counter
-                      hint='More than 6 characters, a-z, at least 1 uppercase and 1 special character'
-                      required
-                      v-model='password'
-                    />
+										<v-text-field
+												label='Password'
+												name='password'
+												type='password'
+												:rules='passwordRules'
+												counter
+												hint='More than 6 characters, a-z, at least 1 uppercase and 1 special character'
+												required
+												v-model='password'/>
 
-                    <v-text-field
-                      id='password'
-                      label='Confirm password'
-                      name='confirm_password'
-                      type='password'
-                      :rules='[
-                        v => v === this.password || &apos;Passwords are not equal&apos;
-                      ]'
-                      hint='More than 6 characters, a-z, at least 1 uppercase and 1 special character'
-                      required
-                    />
-                  </v-form>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn text color='green darken-2' @click='goToLogin'
-                    >Have account? Login</v-btn
-                  >
-                  <v-spacer />
-                  <v-tooltip bottom>
-                    <template v-slot:activator='{ on }'>
-                      <v-btn
-                        text
-                        icon
-                        color='red lighten-2'
-                        v-on='on'
-                        @click='clearForm'
-                      >
-                        <v-icon>mdi-delete</v-icon>
-                      </v-btn>
-                    </template>
-                    <span>Clear the form</span>
-                  </v-tooltip>
-                  <v-btn @click='signUp' :class='{ grey: !valid, green: valid }'
-                    >Sign Up</v-btn
-                  >
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-content>
-    </v-app>
-  </div>
+										<v-text-field
+												id='password'
+												label='Confirm password'
+												name='confirm_password'
+												type='password'
+												:rules='[v => v === this.password || "Passwords are not equal"]'
+												hint='More than 6 characters, a-z, at least 1 uppercase and 1 special character'
+												required/>
+									</v-form>
+								</v-card-text>
+								<v-card-actions>
+									<v-btn text color='green darken-2' @click='goToLogin'>Have account? Login</v-btn>
+									<v-spacer/>
+									<v-tooltip bottom>
+										<template v-slot:activator='{ on }'>
+											<v-btn
+													text
+													icon
+													color='red lighten-2'
+													v-on='on'
+													@click='clearForm'>
+												<v-icon>mdi-delete</v-icon>
+											</v-btn>
+										</template>
+										<span>Clear the form</span>
+									</v-tooltip>
+									<v-btn @click='signUp' :class='{ grey: !valid, green: valid }'>Sign Up</v-btn>
+								</v-card-actions>
+							</v-card>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-content>
+		</v-app>
+	</div>
 </template>
 
 <script>
@@ -166,23 +152,7 @@
 </script>
 
 <style scoped>
-.login {
-  display: block;
-}
-
-input {
-  margin: 10px 0;
-  padding: 15px;
-  border-radius: 4px;
-}
-
-p {
-  margin-top: 40px;
-  font-size: 13px;
-}
-
-p a {
-  text-decoration: underline;
-  cursor: pointer;
-}
+	.login {
+		display: block;
+	}
 </style>
