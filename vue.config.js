25b7bf784f00138f8fd6d88ed35ca2f789197466
Victor Malyshev <vmalyshev0@gmail.com>
2020-01-03 19:05:32 +0200

    Added PWA support and fixed paths

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 module.exports = {
 	lintOnSave: true,
+
 	outputDir: __dirname + '/src/main/webapp/dist',
 	devServer: {
 		proxy: {
@@ -10,6 +14,12 @@ module.exports = {
 			}
 		}
 	},
+	chainWebpack: config => {
+		config.plugin('html').tap(args => {
+			args[0].template = path.resolve(__dirname, 'src/main/webapp/public/index.html');
+			return args;
+		});
+	},
 	configureWebpack: {
 		resolve: {
 			alias: {
@@ -18,11 +28,26 @@ module.exports = {
 		},
 		entry: {
 			app: './src/main/webapp/main.js'
-		}
+		},
+		plugins: [
+			new CopyPlugin([
+				{
+					from: 'src/main/webapp/public/img/icons',
+					to: path.resolve(__dirname, 'src/main/webapp/dist/img/icons'),
+					flatten: true,
+				},
+				{
+					from: 'src/main/webapp/public',
+					to: path.resolve(__dirname, 'src/main/webapp/dist'),
+					flatten: true,
+					ignore: ['index.html', '*.png', '*.jpg']
+				},
+			]),
+		]
 	},
 	pwa: {
 		name: 'Pubs List',
-		themeColor: '#916d13',
+		themeColor: '#1976d2',
 		msTileColor: '#9a9a9a',
 		iconPaths: {
 			msTileImage: 'img/icons/mstile-150x150.png'
